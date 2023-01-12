03611849226e4b168e80b98b5d318b6dc02b82a0
mashashopen <112798347+mashashopen@users.noreply.github.com>
2023-01-12 13:26:34 +0200

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -1,7 +1,7 @@
 
 #create a list of sample IDs
 samples="SRR4064356 SRR4064352 SRR4064357 SRR4064353 SRR4064358 SRR4064354"
-: <<'COMMENT'
+
 #create a directory to save the data
 mkdir data
 
@@ -34,12 +34,12 @@ mkdir fastp_results
 
 for sample in $samples; do
     # run the fastp command
-    /home/mashashopen/fastp --in1 data/$sample\_1.fastq --in2 data/$sample\_2.fastq --out1 fastqc_better_results/$sample\_1.fastp.fastq --out2 fastqc_better_results/$sample\_3.fastp.fastq --unpaired1 fastqc_better_results/$sample\_1.unpaired.fastq --unpaired2 fastqc_better_results/$sample\_2.unpaired.fastq --average_qual 30 --trim_front1 15 --trim_tail1 1 --length_required 60 -O fastqc_better_results
+    /home/mashashopen/fastp --in1 data/$sample\_1.fastq --in2 data/$sample\_2.fastq --out1 fastp_results/$sample\_1.fastp.fastq --out2 fastp_results/$sample\_3.fastp.fastq --unpaired1 fastp_results/$sample\_1.unpaired.fastq --unpaired2 fastp_results/$sample\_2.unpaired.fastq --average_qual 30 --trim_front1 15 --trim_tail1 1 --length_required 60 -O fastqc_better_results
 
 done
 
 
-
+: <<'COMMENT'
 fastp --in1 data/SRR4064356\_1.fastq --in2 data/SRR4064356\_2.fastq --out1 fastqc_better_results/SRR4064356\_1.fastp.fastq --out2 fastqc_better_results/SRR4064356\_2.fastp.fastq --unpaired1 fastqc_better_results/SRR4064356\_1.unpaired.fastq --unpaired2 fastqc_better_results/SRR4064356\_2.unpaired.fastq --average_qual 30 --trim_front1 15 --trim_tail1 1 --length_required 60
 fastp --in1 data/SRR4064352\_1.fastq --in2 data/SRR4064352\_2.fastq --out1 fastqc_better_results/SRR4064352\_1.fastp.fastq --out2 fastqc_better_results/SRR4064352\_2.fastp.fastq --unpaired1 fastqc_better_results/SRR4064352\_1.unpaired.fastq --unpaired2 fastqc_better_results/SRR4064352\_2.unpaired.fastq --average_qual 30 --trim_front1 15 --trim_tail1 1 --length_required 60
 fastp --in1 data/SRR4064357\_1.fastq --in2 data/SRR4064357\_2.fastq --out1 fastqc_better_results/SRR4064357\_1.fastp.fastq --out2 fastqc_better_results/SRR4064357\_2.fastp.fastq --unpaired1 fastqc_better_results/SRR4064357\_1.unpaired.fastq --unpaired2 fastqc_better_results/SRR4064357\_2.unpaired.fastq --average_qual 30 --trim_front1 15 --trim_tail1 1 --length_required 60
