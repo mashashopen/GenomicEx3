2ec2c9a440f2db9e5229e7507dad807e50fa6b71
Masha Shopen <mashashopen@gmail.com>
2023-01-15 23:24:42 +0200

    have vcf

--- myscript.sh
+++ myscript.sh
@@ -1,11 +1,11 @@
 
 #create a list of sample IDs
 samples="SRR4064356 SRR4064352 SRR4064357 SRR4064353 SRR4064358 SRR4064354"
-: <<'COMMENT'
-#create a directory to save the data
+
+#create a directory to save the data # 1
 mkdir data
 
-#iterate over the list of samples # 1
+# iterate over the list of samples and download 10000 reads
 for sample in $samples; do
 	    #run the fastq-dump command
 	        ~/sratoolkit.3.0.1-ubuntu64/bin/fastq-dump -X 10000 --split-files  $sample -O data
@@ -15,8 +15,7 @@ for sample in $samples; do
 # create a directory to save the data # 2
 mkdir fastqc_results
 
-# iterate over the list of samples
-
+# iterate over the list of samples and run fastqc on each
 for sample in $samples; do
     # run the fastqc command
     fastqc data/$sample\_1.fastq -o fastqc_results
@@ -24,40 +23,32 @@ for sample in $samples; do
 
 done
 
-
-
 # create a directory to save the data # 3
 mkdir fastp_results
 
-# iterate over the list of samples
-
-
+# iterate over the list of samples and run fastp to improve the results
 for sample in $samples; do
     # run the fastp command
-    /home/mashashopen/fastp --in1 data/$sample\_1.fastq --in2 data/$sample\_2.fastq --out1 fastqc_better_results/$sample\_1.fastp.fastq --out2 fastqc_better_results/$sample\_3.fastp.fastq --unpaired1 fastqc_better_results/$sample\_1.unpaired.fastq --unpaired2 fastqc_better_results/$sample\_2.unpaired.fastq --average_qual 30 --trim_front1 15 --trim_tail1 1 --length_required 60 -O fastqc_better_results
-
+    ~/fastp --in1 data/$sample\_1.fastq --in2 data/$sample\_2.fastq --out1 fastp_results/$sample\_1.fastp.fastq --out2 fastp_results/$sample\_2.fastp.fastq --unpaired1 fastp_results/$sample\_1.unpaired.fastq --unpaired2 fastp_results/$sample\_2.unpaired.fastq --average_qual 30 --trim_front1 15 --trim_tail1 1 --length_required 60
 done
 
 
-
-fastp --in1 data/SRR4064356\_1.fastq --in2 data/SRR4064356\_2.fastq --out1 fastqc_better_results/SRR4064356\_1.fastp.fastq --out2 fastqc_better_results/SRR4064356\_2.fastp.fastq --unpaired1 fastqc_better_results/SRR4064356\_1.unpaired.fastq --unpaired2 fastqc_better_results/SRR4064356\_2.unpaired.fastq --average_qual 30 --trim_front1 15 --trim_tail1 1 --length_required 60
-fastp --in1 data/SRR4064352\_1.fastq --in2 data/SRR4064352\_2.fastq --out1 fastqc_better_results/SRR4064352\_1.fastp.fastq --out2 fastqc_better_results/SRR4064352\_2.fastp.fastq --unpaired1 fastqc_better_results/SRR4064352\_1.unpaired.fastq --unpaired2 fastqc_better_results/SRR4064352\_2.unpaired.fastq --average_qual 30 --trim_front1 15 --trim_tail1 1 --length_required 60
-fastp --in1 data/SRR4064357\_1.fastq --in2 data/SRR4064357\_2.fastq --out1 fastqc_better_results/SRR4064357\_1.fastp.fastq --out2 fastqc_better_results/SRR4064357\_2.fastp.fastq --unpaired1 fastqc_better_results/SRR4064357\_1.unpaired.fastq --unpaired2 fastqc_better_results/SRR4064357\_2.unpaired.fastq --average_qual 30 --trim_front1 15 --trim_tail1 1 --length_required 60
-fastp --in1 data/SRR4064353\_1.fastq --in2 data/SRR4064353\_2.fastq --out1 fastqc_better_results/SRR4064353\_1.fastp.fastq --out2 fastqc_better_results/SRR4064353\_2.fastp.fastq --unpaired1 fastqc_better_results/SRR4064353\_1.unpaired.fastq --unpaired2 fastqc_better_results/SRR4064353\_2.unpaired.fastq --average_qual 30 --trim_front1 15 --trim_tail1 1 --length_required 60
-fastp --in1 data/SRR4064358\_1.fastq --in2 data/SRR4064358\_2.fastq --out1 fastqc_better_results/SRR4064358\_1.fastp.fastq --out2 fastqc_better_results/SRR4064358\_2.fastp.fastq --unpaired1 fastqc_better_results/SRR4064358\_1.unpaired.fastq --unpaired2 fastqc_better_results/SRR4064358\_2.unpaired.fastq --average_qual 30 --trim_front1 15 --trim_tail1 1 --length_required 60
-fastp --in1 data/SRR4064354\_1.fastq --in2 data/SRR4064354\_2.fastq --out1 fastqc_better_results/SRR4064354\_1.fastp.fastq --out2 fastqc_better_results/SRR4064354\_2.fastp.fastq --unpaired1 fastqc_better_results/SRR4064354\_1.unpaired.fastq --unpaired2 fastqc_better_results/SRR4064354\_2.unpaired.fastq --average_qual 30 --trim_front1 15 --trim_tail1 1 --length_required 60
-COMMENT
-
-
-
-# create a directory to save the data # 2
+# create a directory to save the data # 4
 mkdir fastqc_better_results
 
-# iterate over the list of samples
-
+# iterate over the list of samples and run fastqc to evaluate
 for sample in $samples; do
     # run the fastqc command
     fastqc fastp_results/$sample\_1.fastp.fastq -o fastqc_better_results
 	fastqc fastp_results/$sample\_2.fastp.fastq -o fastqc_better_results
 
+done
+
+samples="2 3 4 6 7 8"
+
+# create VCF files
+mkdir ../VCF/
+for sample in $samples;
+    do
+        bcftools mpileup -f ~/GenomicEx3/DM6Genome/dm6.fa ~/GenomicEx3/data_m/SRR406435$sample.mapped.sorted.bam  | bcftools call -mv -o ../VCF/SRR406435$sample.vcf
 done
